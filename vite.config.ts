170bb509f5415cc47c1c1d02358699cc9ab89f42
ayiezac <afacebu.ayie@gmail.com>
2025-09-03 08:55:42 +0800

    made changes to config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,40 @@
-import autoprefixer from 'autoprefixer';
+import autoprefixer from "autoprefixer";
 // import browserslist from 'browserslist';
-import cssnanoPlugin from 'cssnano';
+import cssnanoPlugin from "cssnano";
 // import { browserslistToTargets } from 'lightningcss';
-import { defineConfig} from 'vite';
-import hmplPlugin from 'vite-plugin-hmpl';
+import { defineConfig } from "vite";
+import hmplPlugin from "vite-plugin-hmpl";
 
 export default defineConfig({
-  base: './',
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['import', 'mixed-decls', 'color-functions', 'global-builtin'],
-      },
-    },
-    transformer: 'lightningcss',
-    postcss: {
-      plugins: [
-        cssnanoPlugin({
-          preset: 'default',
-        }),
-        autoprefixer(),
-      ],
-    },
-  },
-  plugins: [
-    hmplPlugin({
-      memo: true,
-      sanitize: true,
-    }),
-  ],
+	base: "./",
+	css: {
+		modules: {
+			localsConvention: "camelCase",
+		},
+		preprocessorOptions: {
+			scss: {
+				silenceDeprecations: [
+					"import",
+					"mixed-decls",
+					"color-functions",
+					"global-builtin",
+				],
+			},
+		},
+		transformer: "lightningcss",
+		postcss: {
+			plugins: [
+				cssnanoPlugin({
+					preset: "default",
+				}),
+				autoprefixer(),
+			],
+		},
+	},
+	plugins: [
+		hmplPlugin({
+			memo: true,
+			sanitize: true,
+		}),
+	],
 });
